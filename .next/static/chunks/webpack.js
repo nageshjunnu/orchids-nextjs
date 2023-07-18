92cabea9e76516e4e9e7af1f71d3309dee6f04bb
nagesh <nageshalwaystogether@gmail.com>
2023-07-18 19:04:09 +0530

    added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f6a3d395268742d4"; }
+/******/ 		__webpack_require__.h = function() { return "21a517a0174fd2f0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "e981e5b92cf6cacb"; }
+/******/ 	__webpack_require__.h = function() { return "7be97b97f6fa4bc7"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
